4a4eb26b561a38ea0e78e4b80c3463d361d0de2e
Williams Divine-favour Chukwuebuka <134648537+DevBullions@users.noreply.github.com>
2024-04-27 04:14:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -107,7 +107,11 @@ def main():
     st.markdown(html, unsafe_allow_html=True)
     user_query = st.text_input("You:")
 
-    if st.button("Ask"):
+    if st.button("Ask", key="ask_button", help="Click to ask your question", 
+             format_func=lambda _: '<span style="color:white; font-weight:bold;">Ask</span>',
+             help_tooltip="Click to ask your question",
+             key="ask_button",
+             style="height: 40px; width: 150px; background-color: blue; color: white;"):
         if user_query:
             # Retry asking the chatbot in case of errors
             chatbot_response = retry_request(lambda: ask_chatbot(user_query))
